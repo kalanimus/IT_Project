1376078a183ed35acd8f30dc7da45e08aaf33cb2
Kalanimus <nikita.kalan@yandex.ru>
2025-05-20 02:40:01 +0300

    deleted front

--- front_kalm/my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Main from './pages/MainPage/mainPage';
-import App from './appTemp';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App/>
-);
