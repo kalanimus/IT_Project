9c136ed5ccc8cd6cd326d8e13d6d55d509d83b03
Kalanimus <nikita.kalan@yandex.ru>
2025-05-28 22:41:17 +0300

    full back(pls)

--- KALM_Db/Application/DTOs/ReviewDto.cs
+++ KALM_Db/Application/DTOs/ReviewDto.cs
@@ -5,7 +5,7 @@ public class ReviewDto
   public int Id { get; set; }
   public string TeacherFullName { get; set; }
   public string? AuthorFullName { get; set; }
-  public int Rating { get; set; }
+  public double Rating { get; set; }
   public string Text { get; set; }
   public DateTime CreatedAt { get; set; }
   public bool IsAnonymous { get; set; }
