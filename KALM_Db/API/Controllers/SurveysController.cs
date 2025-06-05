c21482234cb918f9e6f3fdacd1e6f8e1376ca604
Nikita Kalanchekaev <nikita.kalan@yandex.ru>
2025-06-05 14:13:06 +0300

    sasd

--- KALM_Db/API/Controllers/SurveysController.cs
+++ KALM_Db/API/Controllers/SurveysController.cs
@@ -190,8 +190,8 @@ namespace API.Controllers
 
             var analytics = await _surveyService.GetAnalyticsAsync(id, group, subject, userName);
             var analyticsDto = _mapper.Map<SurveyAnalyticsDto>(analytics);
-            Console.WriteLine(analytics.Params.ToArray());
-            Console.WriteLine(analyticsDto.Params.ToArray());
+            Console.WriteLine(analytics.Params.Count);
+            Console.WriteLine(analyticsDto.Params.Count);
 
             return Ok(analyticsDto);
         }
